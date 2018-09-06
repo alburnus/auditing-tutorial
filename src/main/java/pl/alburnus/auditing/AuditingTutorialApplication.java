5938c08543b870181bee3d738897b2013fd3f7df
Krzysztof <k.ukleja@gmail.com>
2018-09-06 22:55:23 +0200

    Mock endpoint

--- src/main/java/pl/alburnus/auditing/AuditingTutorialApplication.java
+++ src/main/java/pl/alburnus/auditing/AuditingTutorialApplication.java
@@ -6,12 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import pl.alburnus.auditing.model.Team;
 import pl.alburnus.auditing.repository.TeamRepository;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @SpringBootApplication
 @RestController
 @EnableJpaAuditing
@@ -45,13 +50,34 @@ public class AuditingTutorialApplication {
         return "Delete";
     }
 
+    private List<Team> teams = Arrays.asList(
+            new Team("AA TEAM"),
+            new Team("BB TEAM"),
+            new Team("CC TEAM")
+    );
+
+    @GetMapping("/api/team")
+    public ResponseEntity<List<Team>> getAllMock() {
+        return ResponseEntity.ok(teams);
+    }
+
+    @PostMapping("/api/team")
+    public ResponseEntity<Team> create(@RequestBody Team team) {
+        if(Objects.isNull(team.getName())) {
+            return ResponseEntity.badRequest().build();
+        }
+        List<Team> copyTeams = new ArrayList<>(teams);
+        copyTeams.add(new Team(team.getName()));
+        teams = copyTeams;
+        return ResponseEntity.ok(team);
+    }
+
     // This Bean is required in case when on our Entity we use annotations: @LastModifiedBy and @CreatedBy
     // Documentation: https://docs.spring.io/spring-data/jpa/docs/1.7.0.DATAJPA-580-SNAPSHOT/reference/html/auditing.html
     @Bean
     public AuditorAware<String> createAuditorProvider() {
         return () -> "alburnus";
     }
-
 
 
 }
